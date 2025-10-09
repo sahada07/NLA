a03759be498cd8c22de9393812260d01d0e03f37
Sahada <sahada07@gmail.com>
2025-10-09 10:41:02 +0000

    uncomment

--- NLA/settings.py
+++ NLA/settings.py
@@ -32,18 +32,15 @@ DEBUG = False
 
 # ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = [ '*'
-    # 'localhost',
-    # '127.0.0.1',
-    # 'https://nla-1-jmdg.onrender.com'
+ALLOWED_HOSTS = [ 
+     'localhost',
+     '127.0.0.1',
+    'https://nla-1-jmdg.onrender.com'
 
 
 ]
 
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://nla-1-jmdg.onrender.com',
-    
-# ]
+CSRF_TRUSTED_ORIGINS = ['https://nla-1-jmdg.onrender.com',]
 
 # Application definition
 
